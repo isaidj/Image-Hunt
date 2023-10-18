9d96e2b351b916b3939a20b26c9bd0bd54b6670d
isaidj <isaidj1999@gmail.com>
2023-10-18 17:10:46 -0500

    fix export default imageprops

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageProps } from "@/utils/imagePropsInterface";
+import ImageProps from "@/utils/imagePropsInterface";
 import { IconDown } from "@/assets/Icons";
 // "raw": "https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9",
 //      "full": "https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&fm=jpg&q=80",
