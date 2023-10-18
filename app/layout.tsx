fb470c927786b4bd0750af0b7082472de2072aae
isaidj <isaidj1999@gmail.com>
2023-10-18 08:32:38 -0500

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +17,7 @@ export default function Layout(props: {
     <html lang="en">
       <body className={inter.className}>
         {props.children} {props.modal}
+        <Analytics />
       </body>
     </html>
   );
